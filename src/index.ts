5a8a3937bd2ebdf896881ef880bc25393902d9cb
ViniciusDiefenbach <viniciusdiefenbach2004@gmail.com>
2023-02-11 04:41:01 -0300

    feat: added code style configs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from './server/Server';
 
 server.listen(3333, () => {
-  console.log('app rodando')
+	console.log('app rodando');
 });
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -2,9 +2,13 @@ import express from 'express';
 
 const server = express();
 
+interface Teste {
+
+}
+
 server.get('/', (req, res) => {
-  return res.send('Bom dia, senhor')
-})
+	return res.send('Bom dia, senhor');
+});
 
 export { server };
 
