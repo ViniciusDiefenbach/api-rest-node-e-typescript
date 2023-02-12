2562b051e412b8ee40f2d1969fc8afee82c2318a
ViniciusDiefenbach <viniciusdiefenbach2004@gmail.com>
2023-02-11 22:36:04 -0300

    feat: add CidadesController

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
+import { CidadeController } from '../controllers';
 const router = Router();
 
 router.get('/', (req, res) => {
-	return res.send('Bom dia, senhor');
+  return res.send('Olá, DEV!')
 });
 
 router.post('/teste', (req, res) => {
   return res.json(req.body);
 });
+
+router.post('/cidades', CidadeController.create);
 
 export { router };
 
