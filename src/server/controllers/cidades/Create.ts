12d7664c36da430e788867bee0e9d2d3977709f5
ViniciusDiefenbach <viniciusdiefenbach2004@gmail.com>
2023-02-11 23:51:32 -0300

    feat: add yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,9 +1,27 @@
 import { Request, Response } from 'express';
+import * as yup from 'yup';
 
 interface ICidade {
   nome: string;
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
-  return res.send(req.body);
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+let validatedData: ICidade | undefined = undefined;
+  try{
+    await bodyValidation.validate(req.body);
+    validatedData = req.body
+  } catch(error) {
+    const yupError = error as yup.ValidationError;
+    return res.json({
+      errors: {
+        default: yupError.message,
+      }
+    });
+  }
+
+  return res.send(validatedData);
 }
